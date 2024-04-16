c63153d289e8bb6ea87a50e679588e210e03504d
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-04-16 02:42:06 +0000

    v8

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,23 @@
+import 'package:adv_basics/start_screen.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Image.asset('quiz-logo.png'),
+        body: Container(
+          decoration: const BoxDecoration(gradient: LinearGradient(
+            colors: [
+              Color.fromARGB (255, 50, 14, 111),
+              Color.fromARGB (255, 81, 42, 147),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const StartScreen()),
       ),
     ),
   );
 }
-
-      @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-         title: 'Flutter Demo',
-          theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          //useMaterial3: true,
-        ),
-        //home: Scaffold(
-       // body: Image.asset(name) Image.asset('quiz-logo.png'),
-      //)
-    );
-  }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  
+  const StartScreen({super.key});
+
+  @override
   Widget build(context) {
-    return const Text('Start screen');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png', 
+          width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the Fun Way!', 
+            style: TextStyle(
+              color: Colors.deepOrange, 
+              fontSize: 24
+            ),
+          ),
+          const SizedBox(height: 30),
+          //for button you can go to widget catalog, flutter doc's...
+          OutlinedButton(
+            onPressed: () {}, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz', 
+          ),
+          )
+        ],
+      ),
+    );
   }
 }
 

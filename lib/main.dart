60704e2e228d84018622c4efccac3f36119cb802
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-04-16 01:25:16 +0000

    v5

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-    runApp(
-      MaterialApp(
+void runApp() {
+    
+       MaterialApp(
         home: Scaffold(
           body: Image.asset('quiz-logo.png'),
-      ),
-    ),
-  );
+        ),
+      );
 }
+
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+         title: 'Flutter Demo',
+          theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          //useMaterial3: true,
+        ),
+        //home: Scaffold(
+       // body: Image.asset(name) Image.asset('quiz-logo.png'),
+      //)
+    );
+  }
